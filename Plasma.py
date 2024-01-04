1ac5dd60693724982e2fbd1f7c007d5c51ca758e
Ray <ray.sointula@aalto.fi>
2024-01-05 00:04:27 +0200

    Possibility to save the picture with a button

--- Plasma.py
+++ Plasma.py
@@ -1,8 +1,17 @@
 import tkinter as tk
 from pyfastnoiselite.pyfastnoiselite import FastNoiseLite, NoiseType
+from PIL import Image, ImageGrab
+
+
+def save_image():
+    label_id = label.winfo_id()
+    x, y, w, h = label.winfo_rootx(), label.winfo_rooty(), label.winfo_width(), label.winfo_height()
+    image_data = ImageGrab.grab(bbox=(x, y, x + w, y + h))
+    image_data.save("output_image.png")
+
 
 root = tk.Tk()
-width, height = 500, 500
+width, height = 1650, 500
 image = tk.PhotoImage(width=width, height=height)
 
 label = tk.Label(root, image=image)
@@ -23,23 +32,22 @@ noise_values = [
     for x in range(width)
 ]
 
-# Flatten the noise_values list
 flat_noise_values = [value for row in noise_values for value in row]
 
-# Precompute color values
 color_values = [
     color_format.format(*color) for color in colors
 ]
 
-# Create a list of color indices based on noise values
 color_indices = [
     0 if value > 130 else 1 if 50 < value <= 130 else 2
     for value in flat_noise_values
 ]
 
-# Use the put method with color indices
 for idx, color_index in enumerate(color_indices):
     x, y = divmod(idx, height)
     image.put(color_values[color_index], (x, y))
+
+save_button = tk.Button(root, text="Save Image", command=save_image)
+save_button.pack()
 
 root.mainloop()
